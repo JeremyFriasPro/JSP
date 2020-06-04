0845c4e4fd99442bbfb3f36eb7aba0d7f677037c
Jeremy Frias <jeremyfrias@Jeremys-MBP.home>
2020-06-04 14:12:04 -0400

    Added More Examples For JS Fundamentals

--- Javascript Practice.mjs
+++ Javascript Practice.mjs
@@ -1,7 +1,86 @@
-var worship = "God Is The Best";
-var verdict = true;
-var perfection = 7.0;
-var nb = 8;
+/* var markMass, markHeight;
+var johnMass, johnHeight;
 
-console.log(worship, verdict, perfection);
-console.log(nb);
+markMass = 78;
+markHeight = 1.69;
+
+johnMass = 92;
+johnHeight = 1.95;
+
+var markBmi = markMass / (markHeight * markHeight);
+var johnBmi = johnMass / (johnHeight * johnHeight);
+
+var higherBmi = markBmi > johnBmi;
+
+console.log("Is Mark's Bmi Higher Than John's Bmi? " + higherBmi);
+
+console.log("Mark's BMI is: " + markBmi);
+console.log("John's BMI is: " + johnBmi); */
+
+/*
+var learningRate = 'Great';
+// Ternarary Operator Helps Us not to write an if/else statement
+var learningResult = learningRate === 'Great' ? 'It Says Great' : 'It Says Something Else';
+
+console.log(learningResult);
+
+// Switch Statement
+var weather = 'Sunny';
+
+switch (weather) {
+    case 'Sunny':
+        // We Can Add More Case's To Give Us The Same Result Like
+    case 'Cool':
+        console.log('Let\'s go to the park');
+        break;
+    case 'Rainy':
+        console.log('Let\'s stay indoors');
+        break;
+    case 'Snowing':
+        console.log('Let\'s go snowboarding');
+        break;
+    default:
+        console.log('Enter A Value');
+};
+
+var sub = 4;
+var terSub = sub === 4 ? 'That\'s Right **** ' : 'Try Again';
+console.log(terSub); */
+
+/* 
+var JohnAverage = (125 + 120 + 120) / 3;
+var MaryAverage = (120 + 120 + 120) / 3;
+var MikeAverage = (128 + 120 + 120) / 3;
+
+
+switch (true) {
+    case JohnAverage > MikeAverage && JohnAverage > MaryAverage:
+        console.log("John's Average Points From The Last Three Basketball Games Is Greater Than Mary & Mike's Last Three Games! ");
+        console.log(JohnAverage + " Is John's Basketball Games Average");
+        console.log(MaryAverage + " Is Mary's Basketball Games Average");
+        console.log(MikeAverage + " Is Mike's Basketball Games Average");
+        break;
+    case MikeAverage > JohnAverage && MikeAverage > MaryAverage:
+        console.log("Mike's Average Points From The Last Three Basketball Games Is Greater Than John & Mary's Last Three Games! ");
+        console.log(MikeAverage + " Is Mike's Basketball Games Average");
+        console.log(JohnAverage + " Is Mike's Basketball Games Average");
+        console.log(MaryAverage + " Is Mary's Basketball Games Average");
+        break;
+    case MaryAverage > MikeAverage && MaryAverage > JohnAverage:
+        console.log("Mary's Average Points From The Last Three Basketball Games Is Greater Than Mike & John's Last Three Games! ");
+        console.log(MaryAverage + " Is Mary's Basketball Games Average");
+        console.log(JohnAverage + " Is John's Basketball Games Average");
+        console.log(MikeAverage + " Is Mike's Basketball Games Average");
+        break;
+    case JohnAverage === MikeAverage && JohnAverage === MaryAverage:
+        console.log("It's A Draw!");
+        console.log("John's Average: " + JohnAverage);
+        console.log("Mary's Average: " + MaryAverage);
+        console.log("Mike's Average: " + MikeAverage);
+        break;
+    default:
+        console.log("Check Your Code");
+};
+
+*/
+
